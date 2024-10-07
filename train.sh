2b552a8a10664dc0bb56b5dda3c949a81a7510f5
Ubuntu <ubuntu@ip-172-31-74-77.ec2.internal>
2024-10-07 15:29:15 +0000

    failing at torch.distributed.init_process_group('xla')

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ export XLA_IR_DEBUG=1
 export XLA_HLO_DEBUG=1
 export
 
-export num_workers=2
+export num_workers=1
 
 
 # NEURON_RT_ASYNC_EXEC_MAX_INFLIGHT_REQUESTS=3  NEURON_CC_FLAGS='--cache_dir=./compiler_cache --model-type=cnn-training'  XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 torchrun --nproc_per_node=1 -m yolox.tools.train -n yolox-s -d 1 -b 1 
